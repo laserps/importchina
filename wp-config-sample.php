46a3f7850ec0e6e0c233d85bcf1f99328d4f8186
laserps <mosaklang@gmail.com>
2019-02-20 09:12:17 +0700

    fix hta

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -79,6 +79,10 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+//addition
+define('WP_ALLOW_MULTISITE', true);
+define('CONCATENATE_SCRIPTS', false);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
